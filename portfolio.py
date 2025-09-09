0c3aa20aa13923edb8717d4cc01640546bf6cee4
Claudio E. Enobas Ese <94869159+claudioen@users.noreply.github.com>
2025-09-09 16:30:04 +0100

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -89,7 +89,7 @@ profileHTML=f"""
                             <span class="card-title">About me</span>
                             <p>{profileDescription}</p>
                             <div class="card-action">
-                            <a href="https://www.linkedin.com/in/claudioenobas/" class="blue-text text-darken-3"><i class="fa-brands fa-linkedin fa-2xl"></i></i></a>
+                            <a href="https://www.linkedin.com/in/claudioenobas/" class="blue-text text-darken-3"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
                             <a href="https://github.com/claudioen" class="blue-text text-darken-3"><i class="fa-brands fa-github fa-2xl"></i></a>
                             <a href="https://x.com/" class="blue-text text-darken-3"><i class="fa-brands fa-x-twitter fa-2xl"></i></a>
                             </div>
